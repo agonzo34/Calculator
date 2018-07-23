b6f80213e7c36b08f4c996d0375ff3ea20030b9f
Adam Gonzalez <adam034gonzalez@gmail.com>
2018-07-22 20:44:41 -0400

    Adds 'Calculation' class to project.

---
+++ Calculator/Calculation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator {
+    class Calculation {
+
+        public string inputOne { get; set; }
+        public string inputTwo { get; set; }
+        public Operation operation { get; set; }
+        public bool isInputOne { get; set; }
+
+        public Calculation() {
+            inputOne = "";
+            inputTwo = "";
+            operation = new Operation();
+            isInputOne = true;
+        }
+
+    }
+}
